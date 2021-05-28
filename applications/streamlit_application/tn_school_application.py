b26449b268ec41aabaa7c15e79371cfdd4a88a03
matthgray <matthewlearns2@gmail.com>
2021-05-27 21:21:56 -0500

    update streamlit application for viz and added more data for analysis

--- applications/streamlit_application/tn_school_application.py
+++ applications/streamlit_application/tn_school_application.py
@@ -11,21 +11,38 @@ import numpy as np
 # url
 #url='https://raw.githubusercontent.com/matthgray/nss_capstone/mg_eda/data/clean_data/clean_data.csv'
 tn_data = pd.read_csv('clean_data.csv')
+finance_data = pd.read_csv('clean_county_data.csv')
 
+
+# how to filter by columns?
 # matp
-st.map(tn_data)
+fig_zip = px.scatter_mapbox(finance_data, lat="latitude", lon="longitude",color='Total_Expenditures_Per_School', size="dropout_rate",
+                  color_continuous_scale=px.colors.cyclical.Edge, size_max=15, zoom=5,
+                  mapbox_style="carto-positron")
+fig_zip
+
+
 # filter by county
 sorted_county = sorted(tn_data.district_name.unique())
 selected_from_county =st.sidebar.multiselect('filter by county',sorted_county)
 county_selected = tn_data[(tn_data.district_name.isin(selected_from_county))]
 
-# select columns
-sorted_metric = sorted(tn_data.columns.unique())
-selected_from_metric = st.sidebar.multiselect('choose a metric', sorted_metric)
+# select a metric
+unique_metric = sorted(tn_data.columns.unique())
+selected_metric = st.sidebar.multiselect('choose a metric', unique_metric)
+filter_metric=tn_data[(tn_data.isin(selected_metric))]
+
+#unique_pos = ['RB','QB','WR','FB','TE']
 
 
+# Filtering data
+#df_selected = tn_data[(county_selected) & (filter_metric)]
 
 
-st.dataframe(county_selected)
+figed = px.treemap(county_selected, path=['school_name'], values='dropout_rate',hover_data=['district_name','graduation rate '])
+figed
 
-st.write(county_selected.percent_scoring_21_or_higher.describe(),county_selected.percent_scoring_below_19.describe())
+
+#st.dataframe(df_selected)
+
+#st.write(county_selected.percent_scoring_21_or_higher.describe(),county_selected.percent_scoring_below_19.describe())
