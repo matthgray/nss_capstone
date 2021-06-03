47ed3b941e720b3b1bb45f49ceda197bf8ff54cc
matthgray <matthewlearns2@gmail.com>
2021-06-03 01:32:08 -0500

    new graphs and viz

--- applications/streamlit_application/tn_school_application.py
+++ applications/streamlit_application/tn_school_application.py
@@ -16,6 +16,7 @@ finance_data = pd.read_csv('finance_county_1.csv')
 
 # how to filter by columns?
 # matp
+st.write('''# Schools who spend the most per student and have the largest percentage of funding with local budget ''')
 fig_zip = px.scatter_mapbox(finance_data, lat="latitude", lon="longitude",color='Total_Expenditures_Per_School', size="State_Percentage",
                   color_continuous_scale=px.colors.cyclical.Edge, size_max=15, zoom=5,hover_name="district_name",
                   mapbox_style="carto-positron")
@@ -33,27 +34,30 @@ selected_from_school= st.sidebar.multiselect('filter by school',sort_school,defa
 #select_df=county_selected[(county_selected)&(tn_data[(tn_data.district_name.isin(selected_from_county)))]
 
 
+
 df_selected_school = tn_data[(tn_data.district_name.isin(selected_from_county)) & (tn_data.pool.isin(selected_from_school))]
 
-# select a metric
-unique_metric = sorted(tn_data.columns.unique())
-selected_metric = st.sidebar.multiselect('choose a metric', unique_metric,default=['score_achievement'])
-#filter_metric=county_selected[(county_selected[selected_metric]) & (county_selected[['school_name']])]
 
 
-#st.line_chart(filter_metric)
-
-#unique_pos = ['RB','QB','WR','FB','TE']
-fig = px.bar(df_selected_school, x='pct_chronically_absent_2020', y='school_name')
+st.write('''# Schools with highest percentage of students absent & teachers retained: ''')
+# labels={'pop':'population of Canada'}
+#hover_data=['zipcode']
+fig = px.bar(df_selected_school.sort_values('pct_chronically_absent_2020'), x='pct_chronically_absent_2020', y='school_name',
+              color='percent_retained',hover_data=['zipcode'],
+              labels={'school_name':'SCHOOLS','pct_chronically_absent_2020':'PERCENTAGE OF STUDENTS ABSENT','percent_retained':'PERCENTAGE OF TEACHERS RETAINED'},
+              height=400)
 fig
 
 
-# Filtering data
-#df_selected = tn_data[(county_selected) & (filter_metric)]
+st.write('''# Schools with the highest achievement score from 2019:''')
+figed = px.treemap(df_selected_school, path=['zipcode','school_name'], values='score_achievement',hover_data=['zipcode'])
+figed
 
 
-#figed = px.treemap(df_selected_school, path=['school_name'], values='pct_chronically_absent_2020')
-#figed
+fig_map = px.scatter_mapbox(df_selected_school, lat="latitude", lon="longitude",color='score_achievement', size="score_growth",
+                  color_continuous_scale=px.colors.cyclical.Edge, size_max=15, zoom=5,hover_name="school_name",
+                  mapbox_style="carto-positron")
+fig_map
 #hover_data=['percent_retained','score_achievment']
 
 st.dataframe(df_selected_school)
