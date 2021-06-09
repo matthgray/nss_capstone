7eca728f046348033a264f277d627394ab0b1c81
matthgray <matthewlearns2@gmail.com>
2021-06-09 15:40:47 -0500

    new treemap and changed titles read is better

--- README.md
+++ README.md
@@ -7,7 +7,13 @@ Will schools become a bigger force for where people locate? Will that be a probl
 ### Executive Summary:
 Explore data from Nashville Schools from 2019 and 2020.This will include achievement scores, spending by county, and percent of teachers retained. The 2020 analysis will include high school data for graduate rate, dropout rate, etc. I will create a streamlit application for users to explore the data on their own. This would allow them to filter on the type of school, school name, and provide key statistics for that school. I have made two machine learning model applications. One for classification and one for regression.
 
+### Data:
 https://www.tn.gov/education/data/data-downloads.html
+
+### packages
+* streamlit for application
+* D-Tales for eda and some cleaning
+* Plotly for visualizations
 
 Key Questions to be Answered(KQA)
 * Counties who uses little local finances on PPE and achieves High Achievement scores
--- app/tn_school_application.py
+++ app/tn_school_application.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import seaborn as sns
 import numpy as np
-
+import time
 # read data
 finance_url ='https://raw.githubusercontent.com/matthgray/nss_capstone/omega/data/clean_data/tn_finance_data.csv'
 achievement_url='https://raw.githubusercontent.com/matthgray/nss_capstone/omega/data/clean_data/tn_grade_data.csv'
@@ -17,6 +17,9 @@ finance_data = pd.read_csv(finance_url)
 finance_data['score_achievement']=finance_data['score_achievement'].round(decimals=2)
 # data for ppe and budgets
 tn_data = pd.read_csv(achievement_url)
+# creat a student to teacher ratio column
+tn_data['student_teacher_ratio']=tn_data['students_enrolled'] / tn_data['number_of_teachers']
+tn_data['student_teacher_ratio']=tn_data['student_teacher_ratio'].round(decimals=0)
 #tn_data = tn_data.dropna()
 # fix henderson latitude & longitude
 #finance_data['longitude']= finance_data['longitude'].replace('-88.4053','-86.6200')
@@ -24,7 +27,7 @@ tn_data = pd.read_csv(achievement_url)
 
 #----------------------------------------------------------------#
 # Titles
-
+#st.dataframe(tn_data)
 st.markdown('''# TN SCHOOLS ANALYSIS''')
 st.markdown("""**DATA SOURCE:** [TN.gov](https://www.tn.gov/education/data/data-downloads.html) """)
 st.markdown("""**DEFINITIONS:**[TN.GOV/DEFINITIONS](https://www.tn.gov/content/dam/tn/education/data/data_definitions.pdf)""")
@@ -32,7 +35,7 @@ st.markdown("""**DEFINITIONS:**[TN.GOV/DEFINITIONS](https://www.tn.gov/content/d
 #st.dataframe(tn_data)
 #st.info(tn_data.dtypes)
 #------------------------------------------------------------#
-
+# filters
 #sorted_finance=sorted(finance_data.district_name.unique())
 #sected_from_finance= st.sidebar.multiselect('filter for map',sorted_finance,'Henderson County Schools')
 #sort_finance_df=finance_data[(finance_data.district_name.isin(sected_from_finance))]
@@ -41,7 +44,7 @@ st.markdown("""**DEFINITIONS:**[TN.GOV/DEFINITIONS](https://www.tn.gov/content/d
 # filter by county
 sorted_county = sorted(tn_data.district_name.unique())
 selected_from_county =st.sidebar.multiselect('filter by county',sorted_county,
-default=['Metro Nashville Public Schools','Rutherford County Schools','Williamson County Schools','Wilson County School District','Sumner County Schools','Trousdale County Schools'])
+default=['Metro Nashville Public Schools','Achievement School District','Rutherford County Schools','Williamson County Schools','Wilson County School District','Sumner County Schools','Trousdale County Schools'])
 #county_selected = tn_data[(tn_data.district_name.isin(selected_from_county))]
 
 #select high school or k8
@@ -55,7 +58,7 @@ selected_from_grade= st.sidebar.multiselect('filter by grade',sort_grade,'K8')
 sort_school = sorted(tn_data.school_name.unique())
 selected_from_school= st.sidebar.multiselect('filter by school',sort_school,
 default=['Trousdale Co Elementary','Harpeth Valley Elementary','Springdale Elementary School',
-'Lipscomb Elementary','Lakeside Park Elementary','McFadden School Of Excellence'])
+'Lipscomb Elementary','Lakeside Park Elementary','McFadden School Of Excellence','Corning Achievement Elementary'])
 #'Oak View Elem School','Rucker Stewart Middle','Whitworth-Buchanan Middle School','Watertown Middle School','Stratton Elementary
 #default=['Granbery Elementary','Trousdale Co Elementary','Westover Elementary','Bethesda Elementary','John Pittard Elementary','Watertown Elementary'])
 #select_df=county_selected[(county_selected)&(tn_data[(tn_data.district_name.isin(selected_from_county)))]
@@ -88,24 +91,24 @@ local_fig
 
 
 st.write('''# Schools with the highest achievement score from 2019 by county:''')
-achieve_fig = px.histogram(df_selected_school.sort_values('score_achievement'), x="score_achievement",y='school_name',
-                    color="district_name",title="The schools achievement",
-                    hover_data=['zipcode','district_name','score_achievement'],
-labels={'school_name':'SCHOOLS','score_achievement':'Score achievement  by District','district_name':'District'})
-achieve_fig
+#achieve_fig = px.histogram(df_selected_school.sort_values('score_achievement'), x="score_achievement",y='school_name',
+#                    color="district_name",title="Colors are the districts,x axis is the achievement score of the class, and y axis are the schools in the district",
+#                    hover_data=['zipcode','district_name','score_achievement'],
+#labels={'school_name':'SCHOOLS','score_achievement':'Score achievement  by District','district_name':'District'})
+#achieve_fig
 
+fig = px.treemap(df_selected_school.sort_values('score_achievement'),path =('district_name','pool','school_name'),values = 'score_achievement',
+color = 'district_name',hover_data=['zipcode','district_name','score_achievement','percent_retained'],title="The color shows the district, the size of square is the schools achvievment scores",
+                   labels={'score_achievement':'Achievement Score','district_name':'District','score_achievement':'Achievement Score'})
+fig
 
 
 st.write('''# Achievement score by teacher retention with size of the circle as number of teachers at the school: ''')
-#teacher_fig = px.treemap(df_selected_school, path=['district_name','pool','school_name'], values='percent_retained',
-#                    color='district_name',hover_data=['zipcode','district_name','score_vement'],title="Teacher retention by county",
-#                    labels={'percent_retained':'Percentage of teachers retained','district_name':'District','score_achievement':'Achievement Score'})
 
-#teacher_fig
-teacher_fig=px.scatter(school_selected_df.sort_values('school_name'), x="percent_retained",y='score_achievement',size='number_of_teachers',
+teacher_fig=px.scatter(school_selected_df.sort_values('school_name'), x="percent_retained",y='score_achievement',size='student_teacher_ratio',
                     title="Achievement score by teacher retention with size of the circle as number of teachers at the school",
-                    labels={'percent_retained':'Percentage of teacher retention','score_achievement':'Achievement Score'},
-                    color="school_name",)
+                    labels={'percent_retained':'Percentage of teacher retention','score_achievement':'Achievement Score','school_name':'School'},
+                    color="school_name",hover_data=['percent_ca','percent_retained'])
 
 teacher_fig
 
@@ -123,3 +126,16 @@ student_fig = px.scatter(school_selected_df.sort_values('school_name'), x="perce
                  hover_name="district_name", log_x=True, size_max=60)
 student_fig
 #st.info(df_selected_school.shape)
+#------------------------------------------#
+# recap
+#st.dataframe(df_selected_school)
+if st.button("RECAP"):
+    st.write("# TROUSDALE, SUMNER, WILSON, WILLIAMSON,and RUTHERFORD ARE COMPETING WITH NASHVILLE")
+    st.write("# TROUSDALE IS SPENDING THE LOWEST AND ACHIEVING THE MOST IN MIDDLE TN")
+# final
+if st.button("WHICH COUNTY HAS THE BEST SCHOOLS?"):
+    with st.spinner('Wait for it...'):
+        time.sleep(2)
+    st.balloons()
+    st.success("CONGRATS!")
+    st.write("# WILLIAMSON COUNTY")
